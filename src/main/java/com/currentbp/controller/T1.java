7ba8a585bc76a43f7ed73db2ac08ade82866e850
baopan <baopan@gaosiedu.com>
2018-04-17 14:26:44 +0800

    ss

--- src/main/java/com/currentbp/controller/T1.java
+++ src/main/java/com/currentbp/controller/T1.java
@@ -1,8 +1,11 @@
 package com.currentbp.controller;
 
 
+import com.currentbp.api.daletou.facade.ForecastDaletouServiceFacade;
+import com.currentbp.common.entity.ResultData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/t")
 public class T1 {
     private static Logger logger = LoggerFactory.getLogger(T1.class);
+
+    @Autowired(required = false)
+    private ForecastDaletouServiceFacade forecastDaletouServiceFacade;
+
+
+
+
+
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String sss(){
+    public ResultData sss(){
         logger.info("=====>t1");
-        return null;
+        return ResultData.successed("sssssssss");
     }
 
 }
