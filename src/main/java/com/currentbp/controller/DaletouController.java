59fc1b3ee227a947269967def332005f01d789c8
baopan <baopan@gaosiedu.com>
2018-04-20 18:55:28 +0800

    sss

--- src/main/java/com/currentbp/controller/DaletouController.java
+++ src/main/java/com/currentbp/controller/DaletouController.java
@@ -3,12 +3,18 @@ package com.currentbp.controller;
 import com.currentbp.api.daletou.facade.DaletouServiceFacade;
 import com.currentbp.api.daletou.facade.ForecastDaletouServiceFacade;
 import com.currentbp.common.entity.ResultData;
+import com.currentbp.daletou.bo.entity.DaletouBo;
+import com.currentbp.daletou.condition.DaletouCondition;
+import com.currentbp.daletou.entity.Daletou;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author current_bp
@@ -22,8 +28,41 @@ public class DaletouController {
     @Autowired(required = false)
     private DaletouServiceFacade daletouServiceFacade;
 
+    /**
+     * 查询所有的大乐透列表
+     *
+     * @return 大乐透列表
+     */
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public ResultData all() {
+        return ResultData.successed(daletouServiceFacade.queryDaletouAll());
+    }
+
+    /**
+     * 分页查询
+     *
+     * @return 大乐透列表
+     */
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public ResultData daletouList(){
+    public ResultData list() {
+        DaletouCondition daletouCondition = new DaletouCondition();
+        daletouCondition.setPageNum(1);
+        daletouCondition.setPageSize(10);
+        List<Daletou> daletous = daletouServiceFacade.queryDaletouByCondition(daletouCondition);
+        List<DaletouBo> daletouBos = new ArrayList<>();
+        for (Daletou daletou : daletous) {
+            daletouBos.add(new DaletouBo(daletou));
+        }
+        return ResultData.successed(daletouBos);
+    }
+
+    /**
+     * 预测大乐透
+     *
+     * @return 结果
+     */
+    @RequestMapping(value = "/forecast", method = RequestMethod.GET)
+    public ResultData forecast() {
         return ResultData.successed(daletouServiceFacade.queryDaletouAll());
     }
 }
